7d085b7c47640f92051fdc7f73ef0f6e6cceef38
Lyaguh NoteBook <artyom07206@gmail.com>
2025-02-01 14:08:29 +0300

    Готовый хедер

--- src/components/Buttons/MainButton.tsx
+++ src/components/Buttons/MainButton.tsx
@@ -3,7 +3,7 @@ type LayoutProps = {
 };
 export default function MainButton({ children }: LayoutProps) {
   return (
-    <button className=" font-Golos font-semibold text-base bg-secondColor px-5 py-3 mr-[10px] rounded-lg text-textBlack ">
+    <button className=" font-Golos font-semibold text-base bg-secondColor px-5  mr-[10px] h-11 rounded-lg text-textBlack ">
       {children}
     </button>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -5,12 +5,12 @@ import BlackButton from "../Buttons/BlackButton";
 export default function Header() {
   return (
     <header className=" w-full font-Golos ">
-      <div className="bg-secondColor px-[16px] py-[12px] lg:py-3 lg:px-10 flex justify-between">
-        <div className="basis-3/6 md:basis-2/6 xl:basis-3/6  flex items-center">
+      <div className="bg-secondColor px-[16px] py-[12px] md:px-[72px] lg:py-3 lg:px-10 flex justify-between">
+        <div className="basis-3/6 md:basis-2/6 xl:basis-3/6  flex items-center min-w-[119px]">
           <img src="src\assets\img\Logo.svg" alt="logo" />
         </div>
-        <div className="basis-3/6 justify-end text-textGray pl-3 flex md:pl-0 md:justify-evenly md:basis-4/6 xl:basis-4/6">
-          <div className="flex-wrap  flex w-[180px] lg:w-full lg:max-w-[346px] lg:flex-nowrap lg:justify-between xl:justify-evenly xl:max-w-[460px]">
+        <div className="basis-3/6 justify-end text-textGray  flex md:pl-0 md:justify-evenly md:basis-4/6 xl:basis-4/6">
+          <div className="flex-wrap justify-center bigPhone:justify-normal flex w-[180px] lg:w-full lg:max-w-[346px] lg:flex-nowrap lg:justify-between xl:justify-evenly xl:max-w-[460px]">
             <a href="" className="">
               okoprom@gmail.com
             </a>
@@ -27,15 +27,15 @@ export default function Header() {
         </div>
       </div>
 
-      <nav className="flex justify-between px-10 py-5">
-        <div>
+      <nav className="flex justify-between px-4 py-4 md:flex-wrap md:px-[72px] lg:px-10 lg:py-5">
+        <div className="hidden md:block">
           <MainButton>Главная</MainButton>
           <MainButton>Каталог</MainButton>
           <MainButton>О компании</MainButton>
           <MainButton>Контакты</MainButton>
         </div>
-        <div className="flex justify-evenly basis-1/3">
-          <div>
+        <div className="flex  md:justify-start sm:basis-1/3 md:basis-3/4 lg:justify-evenly lg:basis-2/4 lg:flex-nowrap lg:mt-0 lg:max-w-[420px] md:mt-3">
+          <div className="">
             <NavButton>
               <img src="src\assets\img\Search.svg" alt="" />
             </NavButton>
@@ -43,9 +43,14 @@ export default function Header() {
               <img src="src\assets\img\Cart.svg" alt="" />
             </NavButton>
           </div>
-          <div className="">
+          <div className="hidden md:block">
             <BlackButton>Запросить консультацию</BlackButton>
           </div>
+        </div>
+        <div className="md:hidden">
+          <NavButton>
+            <img src="src\assets\img\menu.svg" alt="" />
+          </NavButton>
         </div>
       </nav>
     </header>
