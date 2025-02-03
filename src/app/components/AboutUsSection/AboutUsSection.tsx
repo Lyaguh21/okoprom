632813fe8c0c8ea059fdc93cdea7bdb9168a135a
Lyaguh NoteBook <artyom07206@gmail.com>
2025-02-03 17:15:22 +0300

    add adaptive

--- src/app/components/AboutUsSection/AboutUsSection.tsx
+++ src/app/components/AboutUsSection/AboutUsSection.tsx
@@ -1,8 +1,8 @@
 export default function AboutUsSection() {
   return (
-    <div className="w-full px-[40px] font-Golos">
-      <div className="flex flex-nowrap">
-        <div className="basis-2/3 p-[40px] rounded-2xl bg-secondColor flex flex-col justify-between">
+    <div className="w-full px-4 bigPhone:px-[40px] font-Golos">
+      <div className="flex flex-wrap xl:flex-nowrap">
+        <div className="xl:basis-2/3 p-[40px] rounded-2xl bg-secondColor flex flex-col justify-between">
           <h1 className="text-textBlack text-[40px] font-normal ">
             Почему именно мы?
           </h1>
@@ -17,23 +17,23 @@ export default function AboutUsSection() {
             использовать то что есть.
           </h4>
         </div>
-        <div className="basis-1/3 min-w-[610px] flex flex-wrap gap-4 pl-4 ">
-          <div className="childrenAbout">
+        <div className=" justify-between mt-4 w-full xl:basis-1/3 xl:min-w-[610px] flex flex-wrap gap-4 xl:pl-4 xl:mt-0 ">
+          <div className="childrenAbout w-[calc(50%-16px)] md:w-[calc(33%-16px)]">
             <img src="\image\IIF-RF.svg" alt="" />
           </div>
-          <div className="childrenAbout">
+          <div className="childrenAbout  w-[calc(50%-16px)] md:w-[calc(33%-16px)]">
             <img src="\image\logo-mkm.svg" alt="" />
           </div>
-          <div className="childrenAbout">
+          <div className="childrenAbout  w-[calc(50%-16px)] md:w-[calc(33%-16px)] ">
             <img src="\image\ollimp.svg" alt="" />
           </div>
-          <div className="childrenAbout">
+          <div className="childrenAbout  w-[calc(50%-16px)] md:w-[calc(33%-16px)] ">
             <img src="\image\VtorTehMix.svg" alt="" />
           </div>
-          <div className="childrenAbout">
+          <div className="childrenAbout  w-[calc(50%-16px)] md:w-[calc(33%-16px)]">
             <img src="\image\UMZ.svg" alt="" />
           </div>
-          <div className="childrenAbout">
+          <div className="childrenAbout  w-[calc(50%-16px)] md:w-[calc(33%-16px)]">
             <img src="\image\VtorCherMet.svg" alt="" />
           </div>
         </div>
