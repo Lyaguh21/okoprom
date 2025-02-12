5d9b46e5d62453c5aefe868cbc0c839ca3a32364
Lyaguh NoteBook <artyom07206@gmail.com>
2025-02-12 21:54:20 +0300

    Slider function

--- Data.js
+++ Data.js
@@ -1,29 +1,29 @@
-export const slider = [
-  {
-    title: `Новый зверь  для всех типов кабелей`,
-    photo: <img src="\image\sliderSection\Cabelerez.png" />,
-    diameter: "от 1,5 до 130мм ",
-    size: "90*68*120 см",
-    power: "3 кВт 380 В",
-    price: "209 000  ₽",
-  },
-  {
-    title: "Тестик 1",
-    photo: <img src="\image\sliderSection\test2.png" />,
-    diameter: "Тестик 1",
-    size: "Тестик 1",
-    power: "Тестик 1",
-    price: "Тестик 1",
-  },
-  {
-    title: "Тестик 2",
-    photo: <img src="\image\sliderSection\test3.png" />,
-    diameter: "Тестик 2",
-    size: "Тестик 2",
-    power: "Тестик 2",
-    price: "Тестик 2",
-  },
-];
+  export const slider = [
+    {
+      title: `Новый зверь  для всех типов кабелей`,
+      photo: <img src="\image\sliderSection\Cabelerez.png" />,
+      diameter: "от 1,5 до 130мм ",
+      size: "90*68*120 см",
+      power: "3 кВт 380 В",
+      price: "209 000  ₽",
+    },
+    {
+      title: "Тестик 1",
+      photo: <img src="\image\sliderSection\test2.png" />,
+      diameter: "Тестик 1",
+      size: "Тестик 1",
+      power: "Тестик 1",
+      price: "Тестик 1",
+    },
+    {
+      title: "Тестик 2",
+      photo: <img src="\image\sliderSection\test3.png" />,
+      diameter: "Тестик 2",
+      size: "Тестик 2",
+      power: "Тестик 2",
+      price: "Тестик 2",
+    },
+  ];
 
 export const back = (
   <svg
--- src/app/components/SliderSection/SliderSection.tsx
+++ src/app/components/SliderSection/SliderSection.tsx
@@ -2,33 +2,62 @@ import MainButton from "../Buttons/MainButton";
 import NavButton from "../Buttons/NavButton";
 import { back, next, slider } from "../../../../Data";
 import Property from "./Property";
+import { useState } from "react";
 
 export default function SliderSection() {
+  const [count, setCount] = useState(0);
+
+  const info = {
+    title: slider[count].title,
+    photo: slider[count].photo,
+    diameter: slider[count].diameter,
+    size: slider[count].size,
+    power: slider[count].power,
+    price: slider[count].price,
+  };
+
+  const updateInfo = (index: number) => {
+    // Проверка на выход за границы массива
+    if (index < 0) {
+      index = slider.length - 1; // Переход к последнему элементу
+    } else if (index >= slider.length) {
+      index = 0; // Переход к первому элементу
+    }
+
+    setCount(index);
+  };
+  function handlePlus() {
+    updateInfo(count + 1);
+  }
+
+  function handleMinus() {
+    updateInfo(count - 1);
+  }
   return (
     <div className="mx-4 md:mx-[72px] lg:mx-[40px] bg-sliderBg font-Golos rounded-2xl p-4">
       <div className="flex-col lg:flex-row flex justify-between">
         <div className="md:basis-1/2 m-[24px] mb-0 border-b-[1px] border-borderBlack">
-          <h1 className="text-white font-semibold text-[40px] leading-[48px] mb-8">
-            {slider[0].title}
+          <h1 className="text-white font-semibold text-[40px] leading-[48px] mb-8 h-[96px]">
+            {info.title}
           </h1>
           <ul className="divide-y divide-borderBlack ">
-            <Property left="Диаметр обработки" right={slider[0].diameter} />
-            <Property left="Размеры" right={slider[0].size} />
-            <Property left="Мощность" right={slider[0].power} />
+            <Property left="Диаметр обработки" right={info.diameter} />
+            <Property left="Размеры" right={info.size} />
+            <Property left="Мощность" right={info.power} />
           </ul>
           <h1 className="mt-[88px] text-white text-[24px] leading-7 mb-[32px]">
-            {slider[0].price}
+            {info.price}
           </h1>
         </div>
         <div className="order-first lg:order-last md:basis-1/2 bg-white dark:bg-darkMainButton rounded-2xl flex justify-center items-center p-[24px]">
-          <picture>{slider[0].photo}</picture>
+          <picture>{info.photo}</picture>
         </div>
       </div>
       <div className="flex justify-between mb-[24px] mx-[24px] mt-[40px]">
         <MainButton>Подробнее</MainButton>
         <div>
-          <NavButton OnClick={() => undefined}>{back}</NavButton>
-          <NavButton OnClick={() => undefined}>{next}</NavButton>
+          <NavButton OnClick={() => handleMinus()}>{back}</NavButton>
+          <NavButton OnClick={() => handlePlus()}>{next}</NavButton>
         </div>
       </div>
     </div>
